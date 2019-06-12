914c61b67ebe08337e8956946a3e60a02a8d5203
hectorhmx <hectorhmx.55@gmail.com>
2019-06-12 01:21:35 -0500

    modified merges

--- Básico/Miercoles/ProgFuncional/LambdaExpressions.py
+++ Básico/Miercoles/ProgFuncional/LambdaExpressions.py
@@ -11,7 +11,12 @@ def sumar(x,y):
 	return x+y
 b=(lambda x,y: x+y)(1,2)
 #####################################################
+def multiplicar(x):
+    return 3*x + 1
 
+g = lambda x: 3*x + 1
+g(2)
+#########################################################
 lista = [2,5,6,12]
 l1=[]
 ########################################################3
@@ -23,7 +28,10 @@ cuadrados()
 
 Cuad = list(map(lambda x:x**2, lista)); print("Funcion usando lambda", Cuad)
 ############################################################
-
+def ec_cuadraticas(a,b,c):
+    return lambda x: a*x**2 + b*x + c
+    
+g = lambda a,b,c,x: a*x**2 + b*x + c
 ############################################################
 def es_par():
 	for i in range(len(lista)):
