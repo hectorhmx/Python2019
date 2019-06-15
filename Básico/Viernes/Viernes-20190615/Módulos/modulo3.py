a2b6d7f1a2b8a5d752480389bb0f6cd02f594fef
hectorhmx <hectorhmx.55@gmail.com>
2019-06-15 14:58:21 -0500

    Added Modules and multy inheritance

---
+++ Básico/Viernes/Viernes-20190615/Módulos/modulo3.py
@@ -0,0 +1,4 @@
+import modulo2 #importamos el módulo 2 de la siguiente manera
+
+print(modulo2.diccionario.items()) # podemos acceder a los elementos de su diccionario
+print(modulo2.suma(2,3,7)) # de esta manera podemos acceder a su función 'suma', la cual recibe 3 parámetros 
---
+++ Básico/Viernes/Viernes-20190615/Módulos/pydoc_ejemplo.py
@@ -0,0 +1,12 @@
+""" Ejemplo para poder documentar mis arhivos de Python"""
+
+class Coche:
+	""" Coche: Clase que posee métodos que describen el comportamiento de un coche"""
+	def avanzar(self):
+		""" Avanzar: Función perteneciente a la clase coche, la cual me imprime en pantalla el sonido que emite un vehículo al circular"""
+		print('Runnn Runnn')
+	def claxon(self):
+		""" claxon: Función perteneciente a coche, la cual imprime en pantalla el sonido que emite el claxon de un vehículo """
+		print('tac tac tac tac tac..')
+
+
